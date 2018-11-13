478c528e6cb85e1090ac389e427e1a7e4145f14f
Olle Eriksson <mail@olle-eriksson.com>
2018-11-13 01:11:51 +0100

    Fixed a loading icon for the observations page

--- src/components/ObservedDsos.tsx
+++ src/components/ObservedDsos.tsx
@@ -7,12 +7,8 @@ import Typography from "@material-ui/core/Typography";
 import Paper from "@material-ui/core/Paper";
 import { IDso, IPagedDsoList } from "../types/Types";
 import Api from "../api/Api";
-// import DsoExtended from "./DsoExtended";
 import BadgedDsoWithObservations from "./BadgedDsoWithObservations";
-import { connect } from "react-redux";
-import { bindActionCreators, Dispatch } from "redux";
-import { IAppState, ReadonlyDataState } from "../types/Types";
-import * as actions from "../actions/SearchActions";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
 const styles = (theme: Theme) => createStyles({
     root: {
@@ -37,18 +33,12 @@ interface IObservedDsosProps extends WithStyles<typeof styles> {
     // Routing
     match: { params: any };
     location: any;
-
-    // Redux
-    store: ReadonlyDataState;
-    actions: any;
-    dispatch?: any;
 }
 
 interface IObservedDsosState {
     isLoading: boolean;
     isError: boolean;
     dsoList: IDso[];
-    moreHits: number;
 }
 
 class ObservedDsos extends React.Component<IObservedDsosProps, IObservedDsosState> {
@@ -56,10 +46,9 @@ class ObservedDsos extends React.Component<IObservedDsosProps, IObservedDsosStat
         super(props);
 
         this.state = {
-            isLoading: false,
+            isLoading: true,
             isError: false,
             dsoList: [],
-            moreHits: 0
         };
     }
 
@@ -71,11 +60,12 @@ class ObservedDsos extends React.Component<IObservedDsosProps, IObservedDsosStat
         Api.getAllDsosAndTheirObservations().then(
             (response) => {
                 const pagedResult: IPagedDsoList = response.data;
-
-                this.setState({ moreHits: pagedResult.more });
+                this.setState({ isLoading: false });
+                this.setState({ isError: false });
                 this.setState({ dsoList: pagedResult.data });
             }).catch(
-                (error) => {
+                () => {
+                    this.setState({ isLoading: false });
                     this.setState({ isError: true });
                 }
             );
@@ -85,15 +75,7 @@ class ObservedDsos extends React.Component<IObservedDsosProps, IObservedDsosStat
         const { classes } = this.props;
 
         let obsList;
-        if (this.state.dsoList.length === 0) {  // No matches
-            obsList = (
-                <Grid key={-1} item={true} xs={12}>
-                    <Typography color="textSecondary" style={{ marginTop: 20 }}>
-                        No matches!
-                    </Typography>
-                </Grid>
-            );
-        } else {
+        if (this.state.dsoList.length > 0) {
             obsList = this.state.dsoList.map(dso => (
                 <Grid item={true} key={dso.id} xs={12}>
                     <Paper className={classes.textfieldPaper} elevation={1}>
@@ -103,36 +85,37 @@ class ObservedDsos extends React.Component<IObservedDsosProps, IObservedDsosStat
             ));
         }
 
-        return <div className={classes.root}>
-            <Typography variant="title" align="center" color="textPrimary" component="p" style={{ marginTop: 20 }}>
-                All observed objects
-            </Typography>
-            <Grid container={true} spacing={40} justify="center" direction="row">
-                <Grid item={true} xs={12} sm={7}>
-                    <Grid container={true} spacing={0} justify="center" direction="column">
-                        {obsList}
+        if (this.state.isLoading) {
+            return (
+                <Grid container={true} spacing={40} justify="center" direction="row">
+                    <Grid item={true} xs={12} sm={7} justify="center" >
+                        <Typography variant="caption" color="textSecondary" gutterBottom={true}>
+                            <CircularProgress />
+                        </Typography>
                     </Grid>
                 </Grid>
-            </Grid>
-        </div>;
+            );
+        } else if (this.state.isError) {
+            return (
+                <Typography color="textSecondary" gutterBottom={true}>
+                    Error!
+                </Typography>
+            );
+        } else {
+            return <div className={classes.root}>
+                <Typography variant="title" align="center" color="textPrimary" component="p" style={{ marginTop: 20 }}>
+                    All observed objects
+                </Typography>
+                <Grid container={true} spacing={40} justify="center" direction="row">
+                    <Grid item={true} xs={12} sm={7}>
+                        <Grid container={true} spacing={0} justify="center" direction="column">
+                            {obsList}
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </div>;
+        }
     }
 }
 
-const mapStateToProps = (state: IAppState) => {
-    return {
-        store: state.data as ReadonlyDataState
-    };
-};
-
-const mapDispatchToProps = (dispatch: Dispatch<actions.SearchAction>) => {
-    return {
-        actions: bindActionCreators(
-            actions,
-            dispatch
-        )
-    };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(withStyles(styles)(ObservedDsos));
-// Non-redux way:
-//export default withStyles(styles)(ObservedDsos);
+export default withStyles(styles)(ObservedDsos);
