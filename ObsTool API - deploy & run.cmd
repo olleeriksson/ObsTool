25ab982705d7468178f1a933cb09a8eaf07a4e22
Olle Eriksson <mail@olle-eriksson.com>
2019-11-11 22:19:45 +0100

    Updated scripts

--- ObsTool API - deploy & run.cmd
+++ ObsTool API - deploy & run.cmd
@@ -5,5 +5,5 @@ echo ############################################################
 
 cd C:\Users\Olle\source\repos\ObsToolApi\ObsTool
 dotnet publish -c Release -r win10-x64
-cd C:\Users\Olle\source\repos\ObsToolApi\ObsTool\bin\Release\netcoreapp2.0\win10-x64\
+cd C:\Users\Olle\source\repos\ObsToolApi\ObsTool\bin\Release\netcoreapp3.0\win10-x64
 .\ObsTool.exe
