05fdfaa82f8524b609a383c7ee09c68a2f8076d2
Olle Eriksson <mail@olle-eriksson.com>
2026-05-19 21:29:41 +0200

    Added more test users

--- ObsTool/Scripts/MYSQL-start-obstool-backend.cmd
+++ ObsTool/Scripts/MYSQL-start-obstool-backend.cmd
@@ -17,6 +17,12 @@ set "Db__ConnectionString=server=127.0.0.1;port=3306;database=obstool;user=obsto
 set "EnableAuthentication=true"
 set "Authentication__Users__0__Username=test"
 set "Authentication__Users__0__HashedPassword=AQAAAAIAAYagAAAAEAhi498U2xmW2gvkYChrI77klzS6xNoJQ1e5KiQubBaLd71GyzbqMT0mpsgFoBin7Q=="
+set "Authentication__Users__1__Username=sagittarius"
+set "Authentication__Users__1__HashedPassword=AQAAAAIAAYagAAAAEAy4Q/bH+H7URRHd6d51Nbg4PQGKAUqHu3ymMJc3s1UE+nTKUv8igrnHbJrctxSb0w=="
+set "Authentication__Users__2__Username=leoträffen"
+set "Authentication__Users__2__HashedPassword=AQAAAAIAAYagAAAAEGezACMMHXz1KgjNO76LqBz9zk7ad7YyrrHc1GzpoGVnnr7LPPBCDiYiCHO1jRF/+Q=="
+set "Authentication__Users__3__Username=leotraffen"
+set "Authentication__Users__3__HashedPassword=AQAAAAIAAYagAAAAEKAcuAgXZPVQCIgwZkFCP0uJHGMLXQ2LYZXdQL5sDB+smLEHGVeji99k6gaSkbqQHg=="
 
 echo(
 echo Login with username "test" and password "test" if authentication is enabled. See EnableAuthentication.
