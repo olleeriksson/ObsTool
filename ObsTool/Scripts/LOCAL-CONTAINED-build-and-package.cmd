cfd644f0d933e66fd0034c4d291c488fb0560061
Olle Eriksson <mail@olle-eriksson.com>
2026-08-15 10:05:29 +0200

    Copy dev database when packaging for contained version

--- ObsTool/Scripts/LOCAL-CONTAINED-build-and-package.cmd
+++ ObsTool/Scripts/LOCAL-CONTAINED-build-and-package.cmd
@@ -4,6 +4,10 @@ cd C:\Users\Olle\source\repos\olleeriksson\ObsTool\ObsTool\
 
 dotnet publish .\ObsTool.csproj --configuration Release --runtime win-x64 --self-contained true --output C:\tmp\ObsTool-portable /p:EnvironmentName=Production
 
+REM Include the current development SQLite database in the portable package.
+if not exist "C:\tmp\ObsTool-portable\Data" mkdir "C:\tmp\ObsTool-portable\Data"
+copy /Y "C:\Users\Olle\source\obstool_database_dev.db" "C:\tmp\ObsTool-portable\Data\obstool_database.db"
+
 copy scripts\Run-ObsTool.cmd C:\tmp\ObsTool-portable\Run-ObsTool.cmd
 
 REM Package the portable build and copy the archive to Google Drive.
